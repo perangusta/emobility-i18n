7b9c0423b0afd20ecf1cef5e9987dedc8ca3679c
Clemens Kofler <clemens@railway.at>
2014-09-04 12:01:52 +0200

    Separator might be passed in as option

--- lib/emobility-i18n/backend/key_prefix.rb
+++ lib/emobility-i18n/backend/key_prefix.rb
@@ -3,13 +3,16 @@ module I18n
     module KeyPrefix
       def translate(locale, key, options = {})
         if key_prefix = options.delete(:key_prefix)
+          separator = options[:separator] || I18n.default_separator
+
           # If the key contains a separator, we want to prepend the prefix only to the last key
-          # part. This is so t(:foo, scope: :bar) and t(:'bar.foo') do the same thing.
-          parts = key.to_s.include?(I18n.default_separator) ?
+          # part. This is so t(:foo, scope: :bar, key_prefix: baz) and
+          # t(:'bar.foo', key_prefix: baz) do the same thing.
+          parts = key.to_s.include?(separator) ?
                     I18n.normalize_keys(locale, key, options[:scope])[1..-1] :
                     [key]
 
-          key = (parts[0..-2] + ["#{key_prefix}_#{parts[-1]}"]).join(I18n.default_separator)
+          key = (parts[0..-2] + ["#{key_prefix}_#{parts[-1]}"]).join(separator)
         end
 
         super(locale, key, options)
