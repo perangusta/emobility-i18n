e01f81eaa2a2e7bec1745b63da5c43306f33839b
Clemens Kofler <clemens@railway.at>
2014-09-04 12:02:43 +0200

    Only extend i18n and i18n config with global scope accessors when global scope module is actually used

--- lib/emobility-i18n.rb
+++ lib/emobility-i18n.rb
@@ -1,29 +1,5 @@
 require 'i18n'
 
-module I18n
-  class Config
-    def global_scope
-      @global_scope
-    end
-
-    def global_scope=(scope)
-      @global_scope = scope
-    end
-  end
-end
-
-module I18n
-  extend Module.new {
-    def global_scope
-      config.global_scope
-    end
-
-    def global_scope=(value)
-      config.global_scope = value
-    end
-  }
-end
-
 require 'emobility-i18n/with_i18n_options'
 
 require 'emobility-i18n/backend/global_scope'
--- lib/emobility-i18n/backend/global_scope.rb
+++ lib/emobility-i18n/backend/global_scope.rb
@@ -1,6 +1,11 @@
 module I18n
   module Backend
     module GlobalScope
+      def self.included(base)
+        I18n::Config.send(:include, ConfigExtension)
+        I18n.extend(CoreExtension)
+      end
+
       def lookup(locale, key, scope = [], options = {})
         return super if I18n.global_scope.nil?
 
@@ -15,6 +20,26 @@ module I18n
           scope = scope.dup
           scope.shift
           super
+        end
+      end
+
+      module ConfigExtension
+        def global_scope
+          @global_scope
+        end
+
+        def global_scope=(scope)
+          @global_scope = scope
+        end
+      end
+
+      module CoreExtension
+        def global_scope
+          config.global_scope
+        end
+
+        def global_scope=(scope)
+          config.global_scope = scope
         end
       end
     end
