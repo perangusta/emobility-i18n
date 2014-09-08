cb951de3d96d40f9bac893e92bc8f450e3acd161
danielgawron <daniel.gawron@netguru.pl>
2014-09-08 08:15:04 +0200

    add default options spec

--- spec/backend/markdown_spec.rb
+++ spec/backend/markdown_spec.rb
@@ -18,40 +18,4 @@ describe I18n::Backend::Markdown do
   it "doesn't render Markdown for non-HTML keys" do
     expect(I18n.translate(:not_markdown)).to eq '**foo**'
   end
-
-  context "when I18n.default_options hash is set" do
-    before do
-      allow(I18n).to receive(:default_options).and_return(a: 1, b: 2)
-    end
-
-    context "when options passed" do
-      it "adds passed options" do
-        expect(I18n).to receive(:merge_with_default_options).and_return(a: 1, b: 2, c: 3)
-        I18n.translate(:not_markdown, c: 3)
-      end
-
-      it "overrides default_options with passed options" do
-        expect(I18n).to receive(:merge_with_default_options).and_return(a: 10, b: 2, c: 3)
-        I18n.translate(:not_markdown, a: 10, c: 3)
-      end
-    end
-
-    context "when options empty" do
-      it "doesn't modify default_options" do
-        expect(I18n).to receive(:merge_with_default_options).and_return(a: 1, b: 2)
-        I18n.translate(:not_markdown)
-      end
-    end
-  end
-
-  context "when I18n.default_options hash is not set" do
-    before do
-      allow(I18n).to receive(:default_options).and_return(nil)
-    end
-
-    it "doesn't modify passed options" do
-      expect(I18n).to receive(:merge_with_default_options).and_return(a: 1, b: 2)
-      I18n.translate(:not_markdown, a: 1, b: 2)
-    end
-  end
 end
---
+++ spec/default_options_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+
+describe "default_options" do
+
+  context "when I18n.default_options hash is set" do
+    before do
+      allow(I18n).to receive(:default_options).and_return(a: 1, b: 2)
+    end
+
+    context "when options passed" do
+      it "adds passed options" do
+        expect(I18n.merge_with_default_options(c: 3)).to eq({ a: 1, b: 2, c:3 })
+      end
+
+      it "overrides default_options with passed options" do
+        expect(I18n.merge_with_default_options(a: 10, c: 3)).to eq({ a: 10, b: 2, c: 3 })
+      end
+    end
+
+    context "when options empty" do
+      it "doesn't modify default_options" do
+        expect(I18n.merge_with_default_options({})).to eq({ a: 1, b: 2 })
+      end
+    end
+  end
+
+  context "when I18n.default_options hash is not set" do
+    before do
+      allow(I18n).to receive(:default_options).and_return(nil)
+    end
+
+    it "doesn't modify passed options" do
+      expect(I18n.merge_with_default_options(a: 1, b: 2)).to eq({ a: 1, b: 2 })
+    end
+  end
+end
