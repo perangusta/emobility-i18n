35fce27c9d6c36cf3a558fe0327960f4dba7b71a
Clemens Kofler <clemens@railway.at>
2014-09-08 18:26:54 +0200

    Cascade by default in eMobility i18n backend

--- lib/emobility-i18n/backend/emobility.rb
+++ lib/emobility-i18n/backend/emobility.rb
@@ -2,6 +2,11 @@ module I18n
   module Backend
     class EMobility < Simple
       include GlobalScopePrefix, Cascade, KeyPrefix, Markdown
+
+      def translate(locale, key, options = {})
+        options.merge!(cascade: true) unless options[:cascade] === false
+        super
+      end
     end
   end
 end
