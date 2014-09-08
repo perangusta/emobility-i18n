e93ea8f788dc699b8be4fafe0b278fa4a5aea45a
Clemens Kofler <clemens@railway.at>
2014-09-08 18:30:28 +0200

    Change include order; add fallbacks module

--- lib/emobility-i18n/backend/emobility.rb
+++ lib/emobility-i18n/backend/emobility.rb
@@ -1,7 +1,7 @@
 module I18n
   module Backend
     class EMobility < Simple
-      include GlobalScopePrefix, Cascade, KeyPrefix, Markdown
+      include GlobalScopePrefix, KeyPrefix, Markdown, Cascade, Fallbacks
 
       def translate(locale, key, options = {})
         options.merge!(cascade: true) unless options[:cascade] === false
