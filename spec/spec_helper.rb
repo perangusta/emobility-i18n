7a2b03f47b5174b2d4013c8436b48956773f9e4c
Clemens Kofler <clemens@railway.at>
2014-09-08 18:27:33 +0200

    Ensure tests are correctly reset

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,4 +6,14 @@ require 'helpers'
 
 RSpec.configure do |config|
   config.include Helpers
+
+  config.before do
+    I18n.default_locale = :en
+    I18n.locale = nil
+  end
+
+  config.after do
+    I18n.global_scope_prefix = nil
+    I18n.global_scope = nil
+  end
 end
